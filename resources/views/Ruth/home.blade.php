1d1f892ad204f31b252124ff4e5e8ecfa1e0867d
RuthMaina <39380759+RuthMaina@users.noreply.github.com>
2018-06-20 10:04:00 +0300

    Update home.blade.php

--- resources/views/Ruth/home.blade.php
+++ resources/views/Ruth/home.blade.php
@@ -9,26 +9,18 @@
 <html>
 <head>
 <title>Home</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="/">Home</a></li>
       <li><a href="/student">Student</a></li>
       <li><a href="/fees">Fees</a></li>
-      <li><a href="#">Page 3</a></li>
     </ul>
   </div>
 </nav>
